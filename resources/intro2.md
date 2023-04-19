41e9e477ae9812a338d1f5a18b979c63e3681ce5
gopal-sakshi <gopal612@gmail.com>
2023-04-19 17:36:38 +0530

    learning graphQL

--- resources/intro2.md
+++ resources/intro2.md
@@ -1,4 +1,6 @@
 graphql
     library that implements the core GraphQL parsing & execution algorithms.
 @apollo/server
-    how to turn HTTP requests and responses into GraphQL operations and run them
+    how to turn HTTP requests & responses into GraphQL operations and run them
+apollo-server-express
+    dont know... it seems used with old versions... so, ignore for NOW...
---
+++ src/index_old.ts
@@ -0,0 +1,41 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone'
+import fs from 'fs';
+import * as resolvers from '../resolvers11.js';
+
+const typeDefs1 = `
+  type Book { title: String author: String }  
+  type Query { books: [Book] }
+`;
+const typeDefs2 = fs.readFileSync('../../schema12.graphql', { encoding: 'utf-8'});
+const resolvers2 = resolvers;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers1 = {
+  Query: { books: () => books, },
+};
+
+const server = new ApolloServer({
+  typeDefs:typeDefs2,
+  resolvers:resolvers2,
+});
+
+// // APPROACH I
+// const { url } = await startStandaloneServer(server, { listen: { port: 3080 } });
+// console.log(`Server listening at: ${url}`);
+
+// APPROACH II
+const url23 = await startStandaloneServer(server, { listen: { port: 3080 } });
+// url23 ========> { url: 'http://localhost:3080/' }
+// console.log(url23);
+console.log(`Server listening at: ${url23.url}`);
